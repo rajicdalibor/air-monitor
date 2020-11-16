c71770b0172dea930fc0414f32bf3179912cf4f1
rajicdalibor <rajicdalibor@yahoo.com>
2020-11-16 15:21:41 +0100

    deploy

---
+++ precache-manifest.6d4c01f1e51d7bb47ca914c606d628bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbf36ff33f4ee819188",
+    "url": "/air-monitor/css/app.8e701582.css"
+  },
+  {
+    "revision": "4b8721fc2a36c5a9502c",
+    "url": "/air-monitor/css/chunk-vendors.1eb310ed.css"
+  },
+  {
+    "revision": "dda0f923d62c2625a84ca6f507b8abdc",
+    "url": "/air-monitor/index.html"
+  },
+  {
+    "revision": "9fbf36ff33f4ee819188",
+    "url": "/air-monitor/js/app.ceb5a94d.js"
+  },
+  {
+    "revision": "4b8721fc2a36c5a9502c",
+    "url": "/air-monitor/js/chunk-vendors.d4859fb0.js"
+  },
+  {
+    "revision": "a1a841f7946d48a7bbcd2201a4076ac8",
+    "url": "/air-monitor/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/air-monitor/robots.txt"
+  }
+]);
